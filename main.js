f9b1876dbc400c51ce4a7fb9c527431fb26da8a8
Alek90 <4lek.90@gmail.com>
2021-10-11 15:36:57 +0200

    Impostata struttura base base.

---
+++ main.js
@@ -0,0 +1,11 @@
+// Domanda quanti km si devono percorrere
+
+
+// Domanda l'età
+
+
+// Calcola costo biglietto
+
+   // Se età < 18 applica 20% sconto
+
+   // Se invece età > 65 applica 40% sconto
