01353e899e26c922e5e583578fd2a283ae2e4eda
Alek90 <4lek.90@gmail.com>
2021-10-11 16:12:02 +0200

    Inserita prima condizione.
    Inserita e testata prima condizione, su console risulta funzionante.

--- main.js
+++ main.js
@@ -8,9 +8,22 @@ console.log(journey);
 const user_age = Number (prompt ("Quanti anni ha?"))
 console.log(user_age);
 
+// Prezzo base
+
+const price_km = 0.21
+
 
 // Calcola costo biglietto
 
+const price_ticket = price_km * journey
+console.log(price_ticket);
+
    // Se età < 18 applica 20% sconto
+   if (user_age < 18) {
+    price_your_ticket = price_ticket * 0.8
+   }
 
    // Se invece età > 65 applica 40% sconto
+
+
+console.log(price_your_ticket);   
