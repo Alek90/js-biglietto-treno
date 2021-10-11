764b0086b42aa7d898c80d3c1248a5030aad5b08
Alek90 <4lek.90@gmail.com>
2021-10-11 15:49:52 +0200

    Inserite le domande da porre all'utente.
    Inserite le domande e verificate su console. Funzionanti.

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // Domanda quanti km si devono percorrere
 
+const journey = prompt ("Quanti km desidera percorrere?")
+console.log(journey);
 
 // Domanda l'età
 
-
+const user_age = prompt ("Quanti anni ha?")
+console.log(user_age);
 // Calcola costo biglietto
 
    // Se età < 18 applica 20% sconto
